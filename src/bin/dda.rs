cc51dacde7002b46298968d038e3042316db60dc
AxoloCrypt <axolocrypt@outlook.com>
2023-02-14 01:12:25 -0600

    DDA algorithm implementation (needs to be fixed)

---
+++ src/bin/dda.rs
@@ -0,0 +1,42 @@
+extern crate minifb;
+
+use minifb::{Window, WindowOptions};
+use graficacion::algorithms::line_dda;
+use graficacion::vertex::Vertex;
+
+const WIDTH: usize = 640;
+const HEIGHT: usize = 360;
+
+
+fn main() {
+
+    let mut buffer = vec![0; WIDTH*HEIGHT];
+    let mut window = Window::new(
+        "Digital Differential Analyzer",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    ).unwrap();
+
+
+    //set limit to 60 fps
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+
+    let vertex1 = Vertex{position: [100.0, 100.0]};
+    let vertex2 = Vertex{position: [100.0, 200.0]};
+
+    let vertex3 = Vertex{position: [200.0, 300.0]};
+    let vertex4 = Vertex{position: [400.0, 200.0]};
+
+
+    while window.is_open() {
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+
+        line_dda(&vertex1, &vertex2, &mut buffer, WIDTH);
+
+        line_dda(&vertex3, &vertex4, &mut buffer, WIDTH);
+
+    }
+
+}
